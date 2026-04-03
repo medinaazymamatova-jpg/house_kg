b753acc70d3e6f2e9d18100211a5ba74b5dec4ed
medinaazymamatova-jpg <medinaazymamatova@gmail.com>
2026-04-03 17:03:19 +0600

    idk

--- my_project/house_app/urls.py
+++ my_project/house_app/urls.py
@@ -11,7 +11,6 @@ router.register(r'images', PropertyImageViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
